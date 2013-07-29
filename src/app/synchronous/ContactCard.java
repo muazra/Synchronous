e8ee8af3b7d7eae1649a17cbee15281468b4de85
Muntaser Ahmed <mra8kf@virginia.edu>
2013-07-29 00:23:37 -0400

    Add Project

---
+++ src/app/synchronous/ContactCard.java
@@ -0,0 +1,22 @@
+package app.synchronous;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ContactCard extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_card);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.contact_card, menu);
+		return true;
+	}
+
+}
