2aa206d24aa2fd47ade144815358834d160d5085
Muaz Rahman <muazra@gmail.com>
2013-07-29 14:28:37 -0400

    Revert Toggle Button
    This reverts commit 0358e9185d3a301a01bef8972c9c92f70afe9e71.

--- src/app/synchronous/ContactCard.java
+++ src/app/synchronous/ContactCard.java
@@ -6,8 +6,12 @@ package app.synchronous;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ContactCard extends Activity {
+
+	int clickCount = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +25,34 @@ public class ContactCard extends Activity {
 		getMenuInflater().inflate(R.menu.contact_card, menu);
 		return true;
 	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		super.onOptionsItemSelected(item);
+		switch(item.getItemId()) {
+		case R.id.toggleButton:	
+			
+			item.setTitle("Toggle Sharing");
+			
+			/*
+			 * Toggle Mechanism: Sharing icon switches images based on parity (evenness or oddness).
+			 * Also, a Toast displays the change in state (Sharing On, Sharing Off). 
+			 */
+
+			clickCount++;
+
+			if ( (clickCount % 2) == 1) {
+				item.setIcon(R.drawable.on);
+				Toast.makeText(getApplicationContext(), "Sharing On", Toast.LENGTH_SHORT).show();
+			}
+			else {
+				item.setIcon(R.drawable.off);
+				Toast.makeText(getApplicationContext(), "Sharing Off", Toast.LENGTH_SHORT).show();
+			}
+			break;
+		}
+		return true;
+	}
+
 
 }
