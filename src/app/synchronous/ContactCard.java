33f1d9ae8ffd60f717dd5abd58166d94b45a6c2a
Muntaser Ahmed <mra8kf@virginia.edu>
2013-07-31 13:53:48 -0400

    Action Bar Icon Toggles Drawer
    Faulty code is still commented out

--- src/app/synchronous/ContactCard.java
+++ src/app/synchronous/ContactCard.java
@@ -6,9 +6,13 @@
 
 package app.synchronous;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
@@ -20,10 +24,13 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class ContactCard extends Activity {
-	
+
 	private DrawerLayout myDrawerLayout;
 	private ListView myDrawerList;
-	
+    private ActionBarDrawerToggle myDrawerToggle;
+
+
+	private String barTitle = "";
 	private String myDrawerTitle = "";
 	private String[] people = {"Person A", "Person B"};
 
@@ -35,19 +42,50 @@ public class ContactCard extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact_card);
 		
+		barTitle = (String) getTitle();
+
 		/**
 		 * NAVIGATION DRAWER
 		 */
-		
+
 		myDrawerTitle = "Available";
 		myDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        myDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-		
-        //myDrawerList.setAdapter(new ArrayAdapter<String>(this,
-        //		R.layout.drawer_list_item, people));
-        
-        //myDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-		
+		myDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+//		myDrawerList.setAdapter(new ArrayAdapter<String>(this,
+//                R.layout.drawer_list_item, people));
+//        myDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+
+		// Action Bar icon toggles Drawer
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		// ActionBarDrawerToggle ties together the the proper interactions
+		// between the sliding drawer and the action bar app icon
+		myDrawerToggle = new ActionBarDrawerToggle(
+				this,                  /* host Activity */
+				myDrawerLayout,         /* DrawerLayout object */
+				R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+				0,  /* USED 0 "open drawer" description for accessibility */
+				0  /* USED 0 "close drawer" description for accessibility */
+				) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(barTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(myDrawerTitle);
+				invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+			}
+		};
+		myDrawerLayout.setDrawerListener(myDrawerToggle);
+
+		if (savedInstanceState == null) {
+			selectItem(0);
+		}
+
 	}
 
 	@Override
@@ -57,25 +95,43 @@ public class ContactCard extends Activity {
 		return true;
 	}
 
+//	@Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        // If the nav drawer is open, hide action items related to the content view
+//        boolean drawerOpen = myDrawerLayout.isDrawerOpen(myDrawerList);
+//        menu.findItem(R.id.editButton).setVisible(!drawerOpen);
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		super.onOptionsItemSelected(item);
 		
 		/**
-		 * ACTION BAR
+		 * NAVIGATION DRAWER
 		 */
 		
+		// The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+       if (myDrawerToggle.onOptionsItemSelected(item)) {
+           return true;
+       }
+
+		/**
+		 * ACTION BAR
+		 */
+
 		switch(item.getItemId()) {
-		
+
 		case R.id.editButton:
 			Intent editIntent = new Intent(this, EditActivity.class);
 			startActivity(editIntent);
 			break;
-		
+
 		case R.id.toggleButton:	
-			
+
 			item.setTitle("Toggle Sharing");
-			
+
 			/*
 			 * Toggle Mechanism: Sharing icon switches images based on parity (evenness or oddness).
 			 * Also, a Toast displays the change in state (Sharing On, Sharing Off). 
@@ -92,12 +148,12 @@ public class ContactCard extends Activity {
 				Toast.makeText(getApplicationContext(), "Sharing Off", Toast.LENGTH_SHORT).show();
 			}
 			break;
-		
+
 		case R.id.feedback_setting:
 			Intent feedbackIntent = new Intent(this, FeedbackActivity.class);
 			startActivity(feedbackIntent);
 			break;
-			
+
 		case R.id.help_setting:
 			Intent helpIntent = new Intent(this, HelpActivity.class);
 			startActivity(helpIntent);
@@ -111,14 +167,28 @@ public class ContactCard extends Activity {
 //            selectItem(position);
 //        }
 //    }
-	
+
 	private void selectItem(int position) {
-		
+
 		// USE FRAGMENT HERE
-		
-//		myDrawerList.setItemChecked(position, true);
-//        myDrawerLayout.closeDrawer(myDrawerList);
-		
+
+		//		myDrawerList.setItemChecked(position, true);
+		//        myDrawerLayout.closeDrawer(myDrawerList);
+
 	}
+	
+//	@Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//        // Sync the toggle state after onRestoreInstanceState has occurred.
+//        myDrawerToggle.syncState();
+//    }
+//
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//        // Pass any configuration change to the drawer toggls
+//        myDrawerToggle.onConfigurationChanged(newConfig);
+//    }
 
 }
