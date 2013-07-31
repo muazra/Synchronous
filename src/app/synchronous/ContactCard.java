88e554f7316e6a04d11a22338a79b5f1c8b2c2d7
Muntaser Ahmed <mra8kf@virginia.edu>
2013-07-31 00:34:56 -0400

    Removed Unused Imports

--- src/app/synchronous/ContactCard.java
+++ src/app/synchronous/ContactCard.java
@@ -5,8 +5,6 @@ package app.synchronous;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
