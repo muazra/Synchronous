f2cd631eb2c1a52f750c1ba6ce1eaec55c462974
Muaz Rahman <muazra@gmail.com>
2014-11-05 01:43:52 -0500

    login activity UI

---
+++ app/src/main/java/com/android/synchronous/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.android.synchronous;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+    }
+
+}
