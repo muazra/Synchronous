355826bb45c17cf2b429365bff3696d91ea5c42a
Muaz Rahman <muazra@gmail.com>
2014-11-06 22:21:29 -0500

    signing up new user parse logic

--- app/src/main/java/com/android/synchronous/NewUserActivity.java
+++ app/src/main/java/com/android/synchronous/NewUserActivity.java
@@ -12,34 +12,47 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import java.io.ByteArrayOutputStream;
 
 public class NewUserActivity extends Activity {
 
     private static final int SELECT_PICTURE = 1;
+
+    private TextView mUsername;
+    private TextView mPassword;
+    private TextView mName;
+    private TextView mEmail;
+    private TextView mPhone;
+    private TextView mCompany;
+    private TextView mTitle;
     private Button mSubmit;
     private Button mUpload;
     private ImageView mImage;
+
+    private ContactModel mContactModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newuser);
 
+        mUsername = (TextView) findViewById(R.id.username_new);
+        mPassword = (TextView) findViewById(R.id.password_new);
+        mName = (TextView) findViewById(R.id.name_new);
+        mEmail = (TextView) findViewById(R.id.email_new);
+        mPhone = (TextView) findViewById(R.id.phone_new);
+        mCompany = (TextView) findViewById(R.id.company_new);
+        mTitle = (TextView) findViewById(R.id.title_new);
+        mUpload = (Button) findViewById(R.id.upload_new);
+        mSubmit = (Button) findViewById(R.id.submit_new);
         mImage = (ImageView) findViewById(R.id.image_new);
 
-        mSubmit = (Button) findViewById(R.id.newuser_submit);
-        mSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), CardActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                startActivity(intent);
-            }
-        });
-
-        mUpload = (Button) findViewById(R.id.upload_new);
         mUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,8 +63,46 @@ public class NewUserActivity extends Activity {
             }
         });
 
+        mContactModel = new ContactModel();
+        submitButton();
+
     }
 
+    private void submitButton(){
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mContactModel.setUsername(mUsername.getText().toString());
+                mContactModel.setPassword(mPassword.getText().toString());
+                mContactModel.setName(mName.getText().toString());
+                mContactModel.setEmail(mEmail.getText().toString());
+                mContactModel.setPhone(mPhone.getText().toString());
+                mContactModel.setCompany(mCompany.getText().toString());
+                mContactModel.setTitle(mTitle.getText().toString());
+
+                ParseUser user = new ParseUser();
+                user.setUsername(mContactModel.getUsername());
+                user.setPassword(mContactModel.getPassword());
+                user.put("contactModel", mContactModel);
+
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            //login successful
+                            Intent intent = new Intent(getApplicationContext(), CardActivity.class);
+                            startActivity(intent);
+                        } else {
+                            //login not successful
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
@@ -71,10 +122,10 @@ public class NewUserActivity extends Activity {
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
                 byte[] byteArray = stream.toByteArray();
+                mContactModel.setPhoto(byteArray);
             }
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
